c38b17da72f500c20790bf602c3753b7ae18f75e
adubov1 <30300237+adubov1@users.noreply.github.com>
2017-07-19 19:07:33 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+var Discordie = require('discordie')
+const Event = Discordie.Events;
+const client = new Discordie();
+
+client.connect({
+  token: 'MzM3Mzc3MDg2NDI1NTMwMzY4.DFF-bw.Q7IqEkdW8hc6hO2XwkU-8DcYME0'
+});
+client.Dispatcher.on(Events.GATEWAY_READY. e => {
+  console.log('Connected as: ' + client.User.Username);
+});
+client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
+if(e.message.content == 'PING') {
+  e.message.channel.sendMessage('PONG');
+}
+
+});
